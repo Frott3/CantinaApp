d61e388ef7eb9514911e43f164d81fc43ed75790
frott3 <Gabriel_frotte@outlook.com>
2018-11-05 09:00:01 -0300

    Sugar ORM

--- app/src/main/java/com/example/aluno/cantina/MainActivity.java
+++ app/src/main/java/com/example/aluno/cantina/MainActivity.java
@@ -6,10 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.orm.SchemaGenerator;
+import com.orm.SugarContext;
+import com.orm.SugarDb;
+
 public class MainActivity extends AppCompatActivity {
 
     Button botaoProd;
     Button botaoQuem;
+    Button botaoSugar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
         botaoProd = (Button) findViewById(R.id.buttonProd);
         botaoQuem = (Button) findViewById(R.id.buttonQuem);
+        botaoSugar = (Button) findViewById(R.id.buttonSugar);
+
 
         botaoProd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +41,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent( MainActivity.this, QuemActivity.class));
             }
         });
+
+        botaoSugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent( MainActivity.this, SugarORMActivity.class));
+            }
+        });
+
+        
+
+        //SugarContext.init(getApplicationContext());
+        //SchemaGenerator schemaGenerator = new SchemaGenerator(this);
+        //schemaGenerator.createDatabase(new SugarDb(this).getDB());
 
     }
 }
---
+++ app/src/main/java/com/example/aluno/cantina/Produto.java
@@ -0,0 +1,36 @@
+package com.example.aluno.cantina;
+
+import com.orm.SugarRecord;
+
+public class Produto extends SugarRecord{
+
+    private String nome;
+    private double valor;
+
+    // Sugar ORM precisa de um construtor vazio
+    // Sem isso, o framework não funciona
+
+    public Produto() {
+
+    }
+
+    // Gets e sets para manipular os atributos privados
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
+
+
